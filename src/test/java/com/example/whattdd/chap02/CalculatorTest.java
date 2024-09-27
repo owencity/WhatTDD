725f7a5e3019de9f1cb4ebfb88022267d21dbaf2
owencity <skyko6530@gmail.com>
2024-09-27 22:23:05 +0900

    start tdd chap02

---
+++ src/test/java/com/example/whattdd/chap02/CalculatorTest.java
@@ -0,0 +1,18 @@
+package com.example.whattdd.chap02;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class CalculatorTest {
+
+    @Test
+    void plus() {
+        int result = Calculator.plus(1,2);
+        assertEquals(3, result);
+        /*
+            1. 항상 메서드 이름에 대한 고민 , 어울리는 영어명칭으로 지을것
+            2. 정적메서드? 인스턴스 메서드? , 메서드를 제공할 클래스 이름은 뭐가좋을지?
+            3. 보통 Red -> Green -> Refacotring 순으로 진행되고 해당 테스트 에러를 실행되게하고 다시 리팩토링한다.
+         */
+    }
+}
