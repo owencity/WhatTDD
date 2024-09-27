34ac535999ef337ba4cd5dc260e65ea3c77dbaa3
owencity <skyko6530@gmail.com>
2024-09-27 21:47:02 +0900

    Initial commit with local changes

---
+++ src/main/java/com/example/whattdd/WhatTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.whattdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatTddApplication.class, args);
+    }
+
+}
